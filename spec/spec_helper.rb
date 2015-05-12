8457a4d75ff76394f2fd1c8a5a4eac29f9a5cf0d
Christopher Thornton <rmdirbin@gmail.com>
2015-05-12 15:23:46 -0700

    Don't run test coverage in 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,14 @@
 require 'rubygems'
 require 'bundler'
-
-Bundler.require :default
-
+require 'pry'
 require 'webmock/rspec'
-require 'codeclimate-test-reporter'
+require 'scep'
 
-WebMock.disable_net_connect!(:allow => 'codeclimate.com')
-CodeClimate::TestReporter.start
+if RUBY_VERSION >= "1.8.7"
+  require 'codeclimate-test-reporter'
+  WebMock.disable_net_connect!(:allow => 'codeclimate.com')
+  CodeClimate::TestReporter.start
+end
 
 def read_fixture(path)
   File.open(fixture_path path).read
